138be32f3c599efeacde746bc61307eb57b0c708
Arman Stepanyan <stepanyan982@gmail.com>
2020-01-11 18:21:32 +0100

    fssff

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -136,4 +136,4 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
